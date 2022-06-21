cf1ee0b4ef5ede9847d1bb5daace46699fe484c4
Christian Caballari <christianccm17@gmail.com>
2022-06-21 11:22:09 -0600

    PU Capicua

---
+++ PU Capicua/Testing.Capicua.Tests/CapicuaServiceTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Testing.PruebaUnitaria;
+using Xunit;
+
+namespace Testing.Capicua.Tests
+{
+    public class CapicuaServiceTests
+    {
+        //Pasa la prueba
+        [Fact]
+        public void TestCapicua202() {
+
+            CapicuaService service = new CapicuaService();
+
+           bool result =  service.VerifyCapicua(202);
+
+            Assert.True(result);
+
+        }
+
+        //Falla la prueba
+        [Fact]
+        public void TestCapicua203()
+        {
+
+            CapicuaService service = new CapicuaService();
+
+            bool result = service.VerifyCapicua(203);
+
+            Assert.True(result);
+
+        }
+
+        //Pasa la prueba
+        [Fact]
+        public void TestCapicua4224()
+        {
+
+            CapicuaService service = new CapicuaService();
+
+            bool result = service.VerifyCapicua(4224);
+
+            Assert.True(result);
+
+        }
+    }
+}
---
+++ PU Capicua/Testing.PruebaUnitaria/CapicuaService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing.PruebaUnitaria
+{
+    public class CapicuaService
+    {
+        public bool VerifyCapicua(int number) {
+
+            bool result = false;
+
+            string numString = number.ToString();
+            char[] numberArray = numString.ToCharArray();
+
+            string numInvertido = string.Empty;
+          
+            for (int index = numString.Length-1; index>= 0; index--) {
+               numInvertido+=numberArray[index];
+            }
+            if (numString.Equals(numInvertido)) {
+                result = true;
+          }
+            return result;
+        
+        }
+    }
+}
